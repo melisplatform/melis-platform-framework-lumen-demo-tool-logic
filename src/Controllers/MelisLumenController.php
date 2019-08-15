75dbaebb53d62862aa04e1c52c10ee323048e3b2
jerremeirago <jrago@melistechnology.com>
2019-08-15 18:57:52 +0800

    added news data

--- src/Controllers/MelisLumenController.php
+++ src/Controllers/MelisLumenController.php
@@ -4,16 +4,25 @@ namespace MelisPlatformFrameworkLumenDemoToolLogic\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
 use MelisPlatformFrameworkLumenDemoToolLogic\Model\MelisDemoAlbumTableLumen;
+use MelisPlatformFrameworkLumen\MelisServices;
 
 class MelisLumenController extends BaseController
 {
+
     public function renderMelisLumen()
     {
         // get all data in demo table
         $data = MelisDemoAlbumTableLumen::all();
+        $melisServices = new MelisServices();
+        $newsSvc = $melisServices->getService('MelisCmsNewsService');
+        $newsData = $newsSvc->getNewsList();
+
+        // view variables
         $viewVariables = [
-            'data' => $data
+            'data' => $data,
+            'newsData' => $newsData
         ];
+
         // getting the view in this module
         return view('MelisPlatformFrameworkLumenDemoToolLogic::melis-lumen', $viewVariables);
     }
--- views/melis-lumen.blade.php
+++ views/melis-lumen.blade.php
@@ -1,6 +1,13 @@
+<style>
+    .tr-table-header {
+        background: #e61c23;
+        color:#fff;
+        line-height: normal;
+    }
+</style>
 <h3>Table melis_demo_album_table_lumen</h3>
 <table class='table'>
-    <tr>
+    <tr class="tr-table-header">
         <th>Id</th>
         <th>Name</th>
         <th>Date</th>
@@ -16,4 +23,35 @@
     @endforeach
 </table>
 
-<h3>News Data</h3>
+<br>
+<br>
+<h3><strong>News Data</strong></h3>
+<p>Sample of rendering the news data from a MelisServices class</p>
+<table class='table'>
+    <tr class='tr-table-header' >
+        <th>ID</th>
+        <th>Status</th>
+        <th>Title</th>
+        <th>Date Created</th>
+        <th>Published Date</th>
+        <th>Unpublished Date</th>
+        <th>Site</th>
+    </tr>
+    @foreach ($newsData as $idx => $val)
+        <tr>
+            <td>{{ $val['cnews_id'] }}</td>
+            <td>
+                @if ($val['cnews_status'])
+                    <i class="fa fa-circle text-success"></i>
+                @else
+                    <i class="fa fa-circle text-danger></i>
+                @endif
+            </td>
+            <td>{{ $val['cnews_title'] }}</td>
+            <td>{{ $val['cnews_creation_date'] }}</td>
+            <td>{{ $val['cnews_publish_date'] }}</td>
+            <td>{{ $val['cnews_unpublish_date'] }}</td>
+            <td>{{ $val['site_label'] }}</td>
+        </tr>
+    @endforeach
+</table>
