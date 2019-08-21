015aa2ae3ac99e0f2b3d2a9c1df21c96db3744e8
jerremeirago <jrago@melistechnology.com>
2019-08-21 17:55:12 +0800

    updated composer

--- src/Controllers/MelisLumenController.php
+++ src/Controllers/MelisLumenController.php
@@ -2,6 +2,7 @@
 
 namespace MelisPlatformFrameworkLumenDemoToolLogic\Controllers;
 
+use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use MelisPlatformFrameworkLumenDemoToolLogic\Model\MelisDemoAlbumTableLumen;
 use MelisPlatformFrameworkLumen\MelisServices;
@@ -9,12 +10,22 @@ use MelisPlatformFrameworkLumen\MelisServices;
 class MelisLumenController extends BaseController
 {
 
+    protected $melisServices;
+
+    public function __construct()
+    {
+        $this->melisServices = new MelisServices();
+    }
+
     public function renderMelisLumen()
     {
         // get all data in demo table
         $data = MelisDemoAlbumTableLumen::all();
-        $melisServices = new MelisServices();
-        $newsSvc = $melisServices->getService('MelisCmsNewsService');
+        // lumen request
+        $request = Request::capture();
+//        $pageId  = $request->request->get('pageId');
+//        $pageData = $this->melisServices->getPageData($pageId);
+        $newsSvc = $this->melisServices->getService('MelisCmsNewsService');
         $newsData = $newsSvc->getNewsList();
 
         // view variables
@@ -26,5 +37,6 @@ class MelisLumenController extends BaseController
         // getting the view in this module
         return view('MelisPlatformFrameworkLumenDemoToolLogic::melis-lumen', $viewVariables);
     }
+
 
 }
