f9ec7e285d8d1cc97c53ba37fcb6faea282cb781
jerremeirago <jrago@melistechnology.com>
2019-10-03 17:25:10 +0800

    fixed plugin no display

--- views/plugins/melis-plugin.blade.php
+++ views/plugins/melis-plugin.blade.php
@@ -1,9 +1,9 @@
 <table class='table'>
     <tr class="tr-table-header">
         <th>Id</th>
-        <th> {{ app('translator')->translate('tr_melis_lumen_table1_heading_name') }}</th>
+        <th> {{ app('ZendTranslator')->translate('tr_melis_lumen_table1_heading_name') }}</th>
         <th>Date</th>
-        <th> {{ app('translator')->translate('tr_melis_lumen_table1_heading_songs') }}</th>
+        <th> {{ app('ZendTranslator')->translate('tr_melis_lumen_table1_heading_songs') }}</th>
     </tr>
     @foreach ($data as $idx => $val)
         <tr>
