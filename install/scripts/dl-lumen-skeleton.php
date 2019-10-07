39bf39d803eb359944a73c4eb99879d2903a066e
Cris <crismakabenta@gmail.com>
2019-10-07 19:56:39 +0800

    Removed scripts

--- install/scripts/dl-lumen-skeleton.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-$isCliReqs = php_sapi_name() == 'cli' ? true : false;
-//third party Lumen
-$thirdPartyFolder = !$isCliReqs ? $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'thirdparty/Lumen' : 'thirdparty/Lumen';
-
-if (!is_dir($thirdPartyFolder))
-    MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('lumen');
