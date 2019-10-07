af24eea9da425897994cd67cbec9d21b3515652a
bryan <rbbrioso28@gmail.com>
2019-10-07 17:54:17 +0800

    Composer script update

--- install/scripts/dl-lumen-skeleton.php
+++ install/scripts/dl-lumen-skeleton.php
@@ -2,8 +2,8 @@
 
 
 $isCliReqs = php_sapi_name() == 'cli' ? true : false;
-//third party file
-$thirdPartyFolder = !$isCliReqs ? $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'thirdparty/Laravel' : 'thirdparty/Lumen';
+//third party Lumen
+$thirdPartyFolder = !$isCliReqs ? $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'thirdparty/Lumen' : 'thirdparty/Lumen';
 
 if (!is_dir($thirdPartyFolder))
     MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('lumen');
