e1e4d75793eee3b7f5c0972bb0139e33c1920cfd
jerremeirago <jrago@melistechnology.com>
2019-11-08 12:34:36 +0800

    placed button on top most right

--- views/lumen-tool/tool-header.blade.php
+++ views/lumen-tool/tool-header.blade.php
@@ -1,13 +1,19 @@
-<div class="lumen-album-head">
+<div class="me-heading bg-white">
     <div class="row">
         <div class="me-hl col-xs-12 col-md-9">
-            <p>{{ __('lumenDemo::translations.tr_melis_lumen_album_header') }}</p>
+            <h1 class="content-heading">{{ __('lumenDemo::translations.tr_melis_lumen_main_heading') }}</h1>
+            <p>{{ __('lumenDemo::translations.tr_melis_lumen_main_sub_heading') }} </p>
         </div>
         <div class="me-hl col-xs-12 col-md-3" align="right">
             <a data-meliskey="lumen_album_list_header_right_add" data-toggle="modal" data-target="#lumenModal"  class="btn btn-success add-lumen-album" title="Add album">
                 <i class="fa fa-plus"></i>
                 {{ __('lumenDemo::translations.tr_melis_lumen_add_button_text') }}
             </a>
+        </div>
+    </div>
+    <div class="row">
+        <div class="me-hl col-xs-12 col-md-9">
+            <p>{{ __('lumenDemo::translations.tr_melis_lumen_album_header') }}</p>
         </div>
     </div>
 </div>
--- views/lumen-tool/tool-main-content.blade.php
+++ views/lumen-tool/tool-main-content.blade.php
@@ -1,22 +1,9 @@
-<?php
-    $namespace = 'MelisPlatformFrameworkLumenDemoToolLogic';
-    /** @var \Zend\Mvc\I18n\Translator $zendTranslator */
-    $zendTranslator = app('ZendTranslator');
-    $tableConfig = config('album_table_config')['table'];
-?>
-<div class="me-heading bg-white">
-    <div class="row">
-        <div class="me-hl col-xs-12 col-sm-12 col-md-12">
-            <h1 class="content-heading">{{ __('lumenDemo::translations.tr_melis_lumen_main_heading') }}</h1>
-            <p>{{ __('lumenDemo::translations.tr_melis_lumen_main_sub_heading') }} </p>
-        </div>
-    </div>
-</div>
+<?php $namespace = 'MelisPlatformFrameworkLumenDemoToolLogic'; ?>
+<!-- header area -->
+@include($namespace . "::lumen-tool/tool-header")
 <div class="innerAll spacing-x2">
-    <!-- header area -->
-    @include($namespace . "::lumen-tool/tool-header")
     {{-- album list --}}
-    <?= app('melisdatatable')->createTable($tableConfig)   ?>
+    <?= app('melisdatatable')->createTable(config('album_table_config')['table']) ?>
     @include($namespace . "::lumen-tool/tool-language-list",$coreLang)
     <!-- temp modal -->
     @include($namespace . "::lumen-tool/temp-modal")
